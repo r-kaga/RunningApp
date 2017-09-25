35e60bae2b492790d350759a6b732f7759d875d7
加賀谷諒 <ryokagaya0624@gmail.com>
2017-09-26 00:46:01 +0900

    アプリサイズの定義ファイル作成

---
+++ RunningApp/AppSize.swift
@@ -0,0 +1,21 @@
+//
+//  AppSize.swift
+//  RunningApp
+//
+//  Created by 加賀谷諒 on 2017/09/25.
+//  Copyright © 2017年 ryo kagaya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppSize {
+    
+    static let height = UIScreen.main.bounds.size.height
+    
+    static let width = UIScreen.main.bounds.size.width
+    
+    static var tabBarHeight: CGFloat { return 49 }
+
+    
+}
