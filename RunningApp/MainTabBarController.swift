c1dd6dda09f117f6afdb9e594f33b4a29bdd6624
加賀谷諒 <ryokagaya0624@gmail.com>
2017-09-26 00:46:38 +0900

    TabBarの追加

---
+++ RunningApp/MainTabBarController.swift
@@ -0,0 +1,42 @@
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController, UITabBarControllerDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        
+        let tabBar = UITabBar()
+        tabBar.barTintColor = .gray
+        UITabBar.appearance().barTintColor = .black
+        UITabBar.appearance().tintColor = .white
+        
+        let vc = Home()
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        let nv = UINavigationController(rootViewController: vc)
+        
+        let vc3 = MyPage()
+        vc3.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        let nv3 = UINavigationController(rootViewController: vc3)
+        
+        let vc2 = News()
+        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 2)
+        let nv2 = UINavigationController(rootViewController: vc2)
+        setViewControllers([nv, nv2, nv3], animated: false)
+    }
+    
+
+}
+
+
+
+class MyTabBar: UITabBar {
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        size.height = 58
+        return size
+    }
+    
+}
