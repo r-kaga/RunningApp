154142f3b8735cbc0ce763749e24df64ca146a14
加賀谷諒 <ryokagaya0624@gmail.com>
2017-09-28 23:51:04 +0900

    mapにpin立て、stopWatch機能

--- RunningApp/WorkController.swift
+++ RunningApp/WorkController.swift
@@ -19,30 +19,30 @@ enum WorkType {
 
 class WorkController: UIViewController {
     
-    var timer: Timer!
+    var startCountTimer: Timer!
     var countImageView: UIImageView!
     var count = 0
+    
+    weak var timer: Timer!
+    var startTime = Date()
     
     var locationManager: CLLocationManager!
 
     @IBOutlet weak var map: UIView!
+    @IBOutlet weak var stopWatchLabel: UILabel!
     
-    var cnt:Double = 0
     var mapView : MKMapView!
-//    var coordinate: CLLocationCoordinate2D!
     
     // 縮尺
-    var latDist : CLLocationDistance = 1000
-    var lonDist : CLLocationDistance = 1000
+    var latDist : CLLocationDistance = 500
+    var lonDist : CLLocationDistance = 500
     
 
 //    let type: WorkType!
-    
 //    init(type: WorkType) {
 //        self.type = type
 //        super.init(nibName: nil, bundle: nil)
 //    }
-//    
 //    required init?(coder aDecoder: NSCoder) {
 //        fatalError("init(coder:) has not been implemented")
 //    }
@@ -73,58 +73,53 @@ class WorkController: UIViewController {
         locationManager = CLLocationManager() // インスタンスの生成
         locationManager.delegate = self // CLLocationManagerDelegateプロトコルを実装するクラスを指定する
         
-//        // セキュリティ認証のステータスを取得.
-//        let status = CLLocationManager.authorizationStatus()
-//        print("authorizationStatus:\(status.rawValue)");
-//
-//        // まだ認証が得られていない場合は、認証ダイアログを表示
-//        // (このAppの使用中のみ許可の設定) 説明を共通の項目を参照
-//        if(status == .notDetermined) {
-//            self.myLocationManager.requestWhenInUseAuthorization()
-//        }
-//
-//        // 取得精度の設定.
-//        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        // 取得頻度の設定.
-//        myLocationManager.distanceFilter = 100
-        
-        
-        // MapViewの生成
-        mapView = MKMapView()
-        mapView.frame = self.view.bounds
-        mapView.delegate = self
-        self.map.addSubview(mapView)
-        
-        let coordinate = locationManager.location?.coordinate
-        
-        // 表示領域を作成
-        let region: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate!, self.latDist, self.lonDist);
-        
-        mapView.setRegion(region, animated: true)  // MapViewに反映
-        
-        let pin: MKPointAnnotation = MKPointAnnotation() // ピンを生成.
-        
-        // 経度、緯度.
-        let myLatitude: CLLocationDegrees = 37.331741
-        let myLongitude: CLLocationDegrees = -122.030333
-        
-        // 中心点.
-        let center: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLatitude, myLongitude)
+        // セキュリティ認証のステータスを取得.
+        let status = CLLocationManager.authorizationStatus()
+        print("authorizationStatus:\(status.rawValue)");
 
-        
-        pin.coordinate = center // 座標を設定.
-        
-        pin.title = "タイトル" // タイトルを設定.
-        pin.subtitle = "サブタイトル"  // サブタイトルを設定.
+        // まだ認証が得られていない場合は、認証ダイアログを表示
+        // (このAppの使用中のみ許可の設定) 説明を共通の項目を参照
+        if (status == .notDetermined) {
+            self.locationManager.requestWhenInUseAuthorization()
+        } else {
+            //
+            //        // 取得精度の設定.
+            //        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+            //        // 取得頻度の設定.
+            //        myLocationManager.distanceFilter = 100
+            
+            
+            // MapViewの生成
+            mapView = MKMapView()
+            mapView.frame = self.view.bounds
+            mapView.delegate = self
+            self.map.addSubview(mapView)
+            
+            let coordinate = locationManager.location?.coordinate
+            mapView.centerCoordinate = coordinate! // mapViewのcenterを現在地に
 
-        mapView.addAnnotation(pin)  // MapViewにピンを追加.
+            // 表示領域を作成
+            let region: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate!, self.latDist, self.lonDist);
+            
+            mapView.setRegion(region, animated: true)  // MapViewに反映
+            
+            let pin: MKPointAnnotation = MKPointAnnotation() // ピンを生成.
+            
+//            let center: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLatitude, myLongitude)
+            pin.coordinate =  coordinate! // 座標を設定.
+            pin.title = "開始位置" // タイトルを設定.
+//            pin.subtitle = "サブタイトル"  // サブタイトルを設定.
+            mapView.addAnnotation(pin)  // MapViewにピンを追加.
+        }
+
     }
 
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.countImageAnimation()
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.countImageAnimation), userInfo: nil, repeats: true)
+        startCountTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.countImageAnimation), userInfo: nil, repeats: true)
     }
     
 
@@ -147,10 +142,52 @@ class WorkController: UIViewController {
             
             if self.count == 4 {
                 self.countImageView.removeFromSuperview()
-                self.timer.invalidate()
+                self.startCountTimer.invalidate()
+                self.startTimer()
             }
         })
 
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.stopTimer()
+    }
+    
+    func startTimer() {
+        if timer != nil{
+            // timerが起動中なら一旦破棄する
+            timer.invalidate()
+        }
+
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(self.timerCounter),
+            userInfo: nil,
+            repeats: true)
+        
+        startTime = Date()
+    }
+    
+    func timerCounter() {
+        // タイマー開始からのインターバル時間
+        let currentTime = Date().timeIntervalSince(startTime)
+        
+        // fmod() 余りを計算
+        let minute = (Int)(fmod((currentTime/60), 60))
+        // currentTime/60 の余り
+        let second = (Int)(fmod(currentTime, 60))
+        
+        // %02d： ２桁表示、0で埋める
+        stopWatchLabel.text = "\(String(format:"%02d", minute)):\(String(format:"%02", second))"
+    }
+    
+    func stopTimer() {
+        if timer != nil{
+            timer.invalidate()
+            stopWatchLabel.text = "00::00:00"
+        }
     }
     
     /** Endボタン押し時
@@ -170,34 +207,38 @@ extension WorkController: CLLocationManagerDelegate {
      */
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
-        case .notDetermined:
-            print("ユーザーはこのアプリケーションに関してまだ選択を行っていません")
-            locationManager.requestWhenInUseAuthorization() // 起動中のみの取得許可を求める
+            case .notDetermined:
+                print("ユーザーはこのアプリケーションに関してまだ選択を行っていません")
+                locationManager.requestWhenInUseAuthorization() // 起動中のみの取得許可を求める
 
-        case .denied:
-            print("ローケーションサービスの設定が「無効」になっています (ユーザーによって、明示的に拒否されています）")
-            // 「設定 > プライバシー > 位置情報サービス で、位置情報サービスの利用を許可して下さい」を表示する
-            break
-        case .restricted:
-            print("このアプリケーションは位置情報サービスを使用できません(ユーザによって拒否されたわけではありません)")
-            // 「このアプリは、位置情報を取得できないために、正常に動作できません」を表示する
-            break
-        case .authorizedAlways:
-            print("常時、位置情報の取得が許可されています。")
-            locationManager.startUpdatingLocation()
+            case .denied:
+                print("ローケーションサービスの設定が「無効」になっています (ユーザーによって、明示的に拒否されています）")
+                // 「設定 > プライバシー > 位置情報サービス で、位置情報サービスの利用を許可して下さい」を表示する
+                break
             
-            /*
-             other その他（デフォルト値）
-             automotiveNavigation 自動車ナビゲーション用
-             fitness 歩行者
-             otherNavigation その他のナビゲーションケース（ボート、電車、飛行機)
-             */
-            locationManager.activityType = .fitness
+            case .restricted:
+                print("このアプリケーションは位置情報サービスを使用できません(ユーザによって拒否されたわけではありません)")
+                // 「このアプリは、位置情報を取得できないために、正常に動作できません」を表示する
+                break
+            
+            case .authorizedAlways:
+                print("常時、位置情報の取得が許可されています。")
+                locationManager.startUpdatingLocation()
+                
+                /*
+                 other その他（デフォルト値）
+                 automotiveNavigation 自動車ナビゲーション用
+                 fitness 歩行者
+                 otherNavigation その他のナビゲーションケース（ボート、電車、飛行機)
+                 */
+                locationManager.activityType = .fitness
 
-        case .authorizedWhenInUse:
-            print("起動時のみ、位置情報の取得が許可されています。")
-            // 位置情報取得の開始処理
-            break
+            case .authorizedWhenInUse:
+                print("起動時のみ、位置情報の取得が許可されています。")
+                locationManager.startUpdatingLocation()
+
+                // 位置情報取得の開始処理
+                break
         }
     }
     
@@ -209,16 +250,15 @@ extension WorkController: CLLocationManagerDelegate {
         
         for location in locations {
             print("緯度:\(location.coordinate.latitude) 経度:\(location.coordinate.longitude) 取得時刻:\(location.timestamp.description)")
-            
-            // 縮尺.
-            let latDist : CLLocationDistance = 100000
-            let lonDist : CLLocationDistance = 100000
-            
+
             // Regionを作成.
-            let region: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, latDist, lonDist);
+            let region: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, self.latDist, self.lonDist);
+            mapView.setRegion(region, animated: true) // MapViewに反映.
             
-            // MapViewに反映.
-            mapView.setRegion(region, animated: true)
+            let pin: MKPointAnnotation = MKPointAnnotation() // ピンを生成.
+            pin.coordinate = location.coordinate // 座標を設定.
+            pin.title = "開始現在" // タイトルを設定.
+            mapView.addAnnotation(pin)  // MapViewにピンを追加.
         }
 
     }
