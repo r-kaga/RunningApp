6db19b4bc8870cbb9df588541eab180b759ee3ca
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-01 17:21:43 +0900

    Swift4 Convert

--- RunningApp/AppDelegate.swift
+++ RunningApp/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = MainTabBarViewController()
         window?.makeKeyAndVisible()
-        
+
         return true
     }
 
