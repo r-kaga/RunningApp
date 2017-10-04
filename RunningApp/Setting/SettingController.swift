56d12e120ad3461896e9c29b70a46c594afc3443
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-04 23:56:12 +0900

    設定Cellをタップで、入力へ

--- RunningApp/Setting/SettingController.swift
+++ RunningApp/Setting/SettingController.swift
@@ -9,21 +9,17 @@
 import Foundation
 import UIKit
 
-class SettingController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+class SettingController: UIViewController, UITableViewDelegate {
     
     
     var TableTitle = [ ["menuTitle01", "title01", "title02"],
                        ["menuTitle02", "title03", "title04"],
-                       
-                       ["menuTitle03", "menuTitle05", "menuTitle06"],
-                       ["menuTitle04", "menuTitle07"]
     ]
-    
     
     var TableSubtitle = [ ["", "subtitle02", "subtitle03"],
                           ["","subtitle05", "subtitle06"],
-                          ["", "subtitle06", "subtitle07"],
-                          ["", "subtitle08"]
+
     ]
 
     override func viewDidLoad() {
@@ -36,33 +32,63 @@ class SettingController: UIViewController, UITableViewDelegate, UITableViewDataS
 		tableView.backgroundColor = .black
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.separatorColor = .black
         self.view.addSubview(tableView)
     }
+    
+    
+    enum SettingType: Int {
+        case weight = 0
+    }
+    
+    
+    func changeSetting(path: Int) {
+        guard let type = SettingType(rawValue: path) else { return }
+
+        let form = SettingForm()
+        present(form, animated: true, completion: nil)
+        
+    }
+    
+
+    
+}
+
+
+extension SettingController: UITableViewDataSource {
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return self.TableTitle.count
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.TableTitle[section].count - 1
     }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
         cell.textLabel?.text = self.TableTitle[indexPath.section][indexPath.row + 1]
         cell.detailTextLabel?.text = self.TableSubtitle[indexPath.section][indexPath.row + 1]
+        cell.backgroundColor = .black
+        cell.textLabel?.textColor = . white
+        cell.detailTextLabel?.textColor = .white
+        
         return cell
     }
+    
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return self.TableTitle[section][0]
     }
     
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        self.changeSetting(path: indexPath.row)
     }
-    
 
     
 }
