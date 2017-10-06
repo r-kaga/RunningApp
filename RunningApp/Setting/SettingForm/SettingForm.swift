306cfa53cea42a4e5bc926aa25cc639369688dc1
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-06 23:03:31 +0900

    validate func定義とheightも登録可能に

--- RunningApp/Setting/SettingForm/SettingForm.swift
+++ RunningApp/Setting/SettingForm/SettingForm.swift
@@ -12,46 +12,61 @@ import UIKit
 
 class SettingForm: UIViewController {
     
-    
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var SettingCategoryLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    var type: Const.SettingType?
+    var categoryName: String!
+    weak var delegate: SettingDelegate?
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tabBarController?.tabBar.isHidden = true
-
+        guard let type = type else { return }
+        switch type {
+            case .weight:
+                categoryName = "weight"
+            case .height:
+                categoryName = "height"
+        }
+        SettingCategoryLabel.text = categoryName
+        
         textField.delegate = self
     }
     
     
     @IBAction func settingButton(_ sender: Any) {
+
+        do {
+            let value = try self.validateSetting(value: textField.text!)
         
-        
-        guard let text = self.textField.text, !text.isEmpty
-            else {
-                let alert = UIAlertController(title: "からです", message: "からです", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                    print("OK")
-                }))
-                self.present(alert, animated: true, completion: nil)
-                return
+            self.dismiss(animated: true, completion: {
+                UserDefaults.standard.set(String(value), forKey: self.categoryName)
+                self.delegate?.reload()
+            })
+        } catch {
+            
         }
-        
-        guard let weight = Int(text) else {
-            let alert = UIAlertController(title: "数値を入れて下さい", message: "数値です", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                print("OK")
-            }))
-            self.present(alert, animated: true, completion: nil)
-            return
-        }
-        
-        self.dismiss(animated: true, completion: {
-            UserDefaults.standard.set(String(weight), forKey: "weight")
-        })
+//        }
+//        catch Const.ErrorType.empty {
+//
+//            let alert = UIAlertController(title: "からです", message: "からです", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+//                print("OK")
+//            }))
+//            self.present(alert, animated: true, completion: nil)
+//
+//        } catch Const.ErrorType.notInteger {
+//            let alert = UIAlertController(title: "数値を入れて下さい", message: "数値です", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+//                print("OK")
+//            }))
+//            self.present(alert, animated: true, completion: nil)
+//        } catch {
+//            print("unexpected")
+//        }
         
     }
 
@@ -60,6 +75,38 @@ class SettingForm: UIViewController {
             
         }
     }
+    
+    
+    private func validateSetting(value: String) throws -> Int  {
+        
+//        do {
+        guard !value.isEmpty else {
+            throw Const.ErrorType.empty
+        }
+        
+        guard let value = Int(value) else {
+            throw Const.ErrorType.notInteger
+        }
+        return value
+//
+//        } catch Const.ErrorType.empty {
+//
+//            let alert = UIAlertController(title: "からです", message: "からです", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+//                print("OK")
+//            }))
+//            self.present(alert, animated: true, completion: nil)
+//
+//        } catch Const.ErrorType.notInteger {
+//            let alert = UIAlertController(title: "数値を入れて下さい", message: "数値です", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+//                print("OK")
+//            }))
+//            self.present(alert, animated: true, completion: nil)
+//        }
+        
+    }
+    
     
 }
 
