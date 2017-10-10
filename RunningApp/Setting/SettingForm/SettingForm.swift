cccb475a2b1890786bc03f43750186281f40f5df
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-10 21:20:27 +0900

    SettingButtonClassを移動

--- RunningApp/Setting/SettingForm/SettingForm.swift
+++ RunningApp/Setting/SettingForm/SettingForm.swift
@@ -131,15 +131,4 @@ extension SettingForm: UITextFieldDelegate {
 }
 
 
-class SettingButton: UIButton {
-    
-    @IBInspectable var cornerRadius: CGFloat = 10.0
-    
-    override func draw(_ rect: CGRect) {
-        layer.cornerRadius = cornerRadius
-        clipsToBounds = true
-    }
-    
-}
-
 
