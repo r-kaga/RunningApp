ef74b9ae1802ddae583406b3cc04ee17c18c8b2a
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-21 00:21:54 +0900

    AppSizeファイル修正

--- RunningApp/AppSize.swift
+++ RunningApp/AppSize.swift
@@ -11,6 +11,8 @@ import UIKit
 
 struct AppSize {
     
+    static let bounds = UIScreen.main.bounds
+
     static let height = UIScreen.main.bounds.size.height
     
     static let width = UIScreen.main.bounds.size.width
@@ -18,6 +20,19 @@ struct AppSize {
     static let center = CGPoint(x: AppSize.width / 2, y: AppSize.height / 2)
     
     static var tabBarHeight: CGFloat { return 49 }
+    
+    static var toolBarHeight: CGFloat { return 44 }
 
+    static let statusBarHeight = UIApplication.shared.statusBarFrame.height
+    
+    static var navigationBarHeight: CGFloat { return 44 }
+
+//    static let navigationBarHeight = { (navigationController: UINavigationController) -> CGFloat in
+//        return navigationController.navigationBar.frame.size.height
+//    }
+ 
+    static var statusBarAndNavigationBarHeight: CGFloat { return statusBarHeight + navigationBarHeight }
+
+    
     
 }
