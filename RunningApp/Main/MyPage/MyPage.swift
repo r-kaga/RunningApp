53a09ba949000001bbf277675abf30174a21c8e0
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-22 00:41:36 +0900

    Mypage作成

--- RunningApp/Main/MyPage/MyPage.swift
+++ RunningApp/Main/MyPage/MyPage.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import RealmSwift
 
-class MyPage: UIViewController {
+class MyPage: UIViewController, UIScrollViewDelegate {
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = UIColor.red
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "My Page"
+        
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: AppSize.navigationBarHeight + 30,
+                                                  width: AppSize.width, height: AppSize.height))
+        scrollView.backgroundColor = .black
+//        scrollView.isPagingEnabled = true
+        scrollView.bounces = false
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.delegate = self
+  
+        
+        let realm = try! Realm()
+        let data = realm.objects(RealmDataSet.self).sorted(byKeyPath: "id", ascending: false)
+        
+        var count: CGFloat = 0
+        data.forEach { (value) in
+            let view = resultView(frame: CGRect(x: 15,
+                                              y: (AppSize.height / 2.3) * count,
+                                                width: AppSize.width - 30,
+                                                height: AppSize.height / 2.5))
+            
+            view.setValueToResultView(dateTime: value.date,
+                                      timeValue: value.time,
+                                      distance: value.distance,
+                                      speed: value.speed,
+                                      calorie: value.calorie)
+            scrollView.addSubview(view)
+            
+            count += 1
+        }
+        
+        scrollView.contentSize = CGSize(width: AppSize.width, height: (AppSize.height / 2.3 + 5) * count) // 中身の大きさを設定
+        self.view.addSubview(scrollView)
+        
     }
 
 
