b65facbfb337ba390c9b8f04f37c99dfe26f72c9
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-22 12:47:03 +0900

    MyPageに表示する結果のViewを修正

--- RunningApp/Main/MyPage/MyPage.swift
+++ RunningApp/Main/MyPage/MyPage.swift
@@ -33,9 +33,9 @@ class MyPage: UIViewController, UIScrollViewDelegate {
         var count: CGFloat = 0
         data.forEach { (value) in
             let view = resultView(frame: CGRect(x: 15,
-                                              y: (AppSize.height / 2.3) * count,
+                                              y: 190 * count,
                                                 width: AppSize.width - 30,
-                                                height: AppSize.height / 2.5))
+                                                height: 175))
             
             view.setValueToResultView(dateTime: value.date,
                                       timeValue: value.time,
@@ -47,7 +47,7 @@ class MyPage: UIViewController, UIScrollViewDelegate {
             count += 1
         }
         
-        scrollView.contentSize = CGSize(width: AppSize.width, height: (AppSize.height / 2.3 + 5) * count) // 中身の大きさを設定
+        scrollView.contentSize = CGSize(width: AppSize.width, height: (195 * count) + 20 ) // 中身の大きさを設定
         self.view.addSubview(scrollView)
         
     }
