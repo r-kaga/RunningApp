41af05289cba5c36333e05748f895c3cafbf0631
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-24 23:12:43 +0900

    extensionファイル追加

---
+++ RunningApp/Service/Extension/UIView+.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+.swift
+//  RunningApp
+//
+//  Created by 加賀谷諒 on 2017/10/24.
+//  Copyright © 2017年 ryo kagaya. All rights reserved.
+//
+
+import Foundation
---
+++ RunningApp/Service/Extension/UIViewController+.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+.swift
+//  RunningApp
+//
+//  Created by 加賀谷諒 on 2017/10/24.
+//  Copyright © 2017年 ryo kagaya. All rights reserved.
+//
+
+import Foundation
