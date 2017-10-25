882467b426db1cc20f189a3f53c0ec8f9c10dbe9
加賀谷諒 <ryokagaya0624@gmail.com>
2017-10-25 16:46:23 +0900

    tabにMypageアイコン設定

--- RunningApp/MainTabBarController.swift
+++ RunningApp/MainTabBarController.swift
@@ -13,15 +13,15 @@ class MainTabBarViewController: UITabBarController, UITabBarControllerDelegate {
         UITabBar.appearance().tintColor = .white
         
         let vc = Home()
-        vc.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "account")!, tag: 1)
+        vc.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "home")!, tag: 1)
         let nv = UINavigationController(rootViewController: vc)
         
         let vc2 = MyPage()
-        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 2)
+        vc2.tabBarItem = UITabBarItem(title: "MyPage", image: UIImage(named: "account")!, tag: 2)
         let nv2 = UINavigationController(rootViewController: vc2)
         
         let vc3 = SettingController()
-        vc3.tabBarItem = UITabBarItem(title: "Setting", image: UIImage(named: "Setting")!, tag: 2)
+        vc3.tabBarItem = UITabBarItem(title: "Setting", image: UIImage(named: "setting")!, tag: 3)
         let nv3 = UINavigationController(rootViewController: vc3)
 
         setViewControllers([nv, nv2 ,nv3], animated: true)
