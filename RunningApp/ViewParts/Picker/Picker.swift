623347790dc12a94f3e93dc9e6d81522c7d12496
加賀谷諒 <ryokagaya0624@gmail.com>
2017-11-17 19:14:32 +0900

    pickerPartsを作成

---
+++ RunningApp/ViewParts/Picker/Picker.swift
@@ -0,0 +1,21 @@
+//
+//  Picker.swift
+//  RunningApp
+//
+//  Created by 加賀谷諒 on 2017/11/17.
+//  Copyright © 2017年 ryo kagaya. All rights reserved.
+//
+
+import UIKit
+
+class Picker: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
