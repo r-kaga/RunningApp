09346bf4658f4512b5749bacfcc1a00dd0008991
加賀谷諒 <ryokagaya0624@gmail.com>
2017-11-17 19:24:45 +0900

    borderをつけるExtension追加

--- RunningApp/Service/Extension/UIView+.swift
+++ RunningApp/Service/Extension/UIView+.swift
@@ -12,7 +12,54 @@ import UIKit
 
 extension UIView {
     
-
+    enum BorderType {
+        case Top
+        case Left
+        case Right
+        case Bottom
+    }
+    /** UIViewにボーダをつける
+     * @param aTypes - ボーダをつける面リスト[.Top, .Left, .Right, .Bottom]
+     * @param aWidth - ボーダの太さ
+     * @param aColor - ボーダカラー
+     */
+    func addBorder(types aTypes: [BorderType], width aWidth: CGFloat = 1.0, color aColor: UIColor) {
+        for type in aTypes {
+            let border = CALayer()
+            border.backgroundColor = aColor.cgColor
+            switch type {
+            case .Top :
+                border.frame = CGRect(
+                    x: 0,
+                    y: 0,
+                    width: self.frame.size.width,
+                    height: aWidth
+                )
+            case .Left :
+                border.frame = CGRect(
+                    x: 0,
+                    y: 0,
+                    width: aWidth,
+                    height: self.frame.size.height
+                )
+            case .Right :
+                border.frame = CGRect(
+                    x: self.frame.size.width,
+                    y: 0,
+                    width: aWidth,
+                    height: self.frame.size.height
+                )
+            case .Bottom :
+                border.frame = CGRect(
+                    x: 0,
+                    y: self.frame.size.height,
+                    width: self.frame.size.width,
+                    height: aWidth
+                )
+            }
+            self.layer.addSublayer(border)
+        }
+    }
     
 }
 
